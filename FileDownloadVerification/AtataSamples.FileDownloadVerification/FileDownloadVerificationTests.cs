48f471ba416142b556d06b63cf8bf80812f0cffc
Yaroslav Samus <yaroslav.samus@relativity.com>
2020-04-27 18:54:03 +0300

    Rename AtataReleaseListGithubPage page object to ReleaseGitHubPage; Add FileDownloadVerification test; Add Selenium.WebDriver.ChromeDriver nuget package.

---
+++ FileDownloadVerification/AtataSamples.FileDownloadVerification/FileDownloadVerificationTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using Atata;
+using NUnit.Framework;
+
+namespace AtataSamples.FileDownloadVerification
+{
+	[TestFixture]
+	public class FileDownloadVerificationTests
+	{
+		private string DownloadDirectoryPath { get; } = $"{AppDomain.CurrentDomain.BaseDirectory}/OutputFiles";
+
+		[SetUp]
+		public void SetUp()
+		{
+			SetUpDownloadDirectory();
+
+			AtataContext.Configure().
+				UseChrome().
+					//WithArguments("start-maximized").
+					//WithArguments("headless").
+					WithOptions(options => options.AddUserProfilePreference("download.default_directory", DownloadDirectoryPath)).
+					WithLocalDriverPath().
+				UseBaseUrl("https://github.com/").
+				UseCulture("en-us").
+				UseAllNUnitFeatures().
+				Build();
+		}
+
+		private void SetUpDownloadDirectory()
+		{
+			if (Directory.Exists(DownloadDirectoryPath))
+				Directory.Delete(DownloadDirectoryPath);
+
+			Directory.CreateDirectory(DownloadDirectoryPath);
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			AtataContext.Current?.CleanUp();
+		}
+
+		[Test]
+		public void FileDownloadVerification()
+		{
+			Go.To<ReleaseGitHubPage>(url: "atata-framework/atata/releases/tag/v1.5.0").
+				SourceCodeZipLink.Click();
+
+			//string sourceCodeZipExpectedPath = Path.Combine(DownloadDirectoryPath, "atata-1.5.0.zip");
+		}
+	}
+}
---
+++ FileDownloadVerification/AtataSamples.FileDownloadVerification/ReleaseGitHubPage.cs
@@ -0,0 +1,12 @@
+﻿using Atata;
+
+namespace AtataSamples.FileDownloadVerification
+{
+	using _ = ReleaseGitHubPage;
+
+	public class ReleaseGitHubPage : Page<_>
+	{
+		[FindByContent("Source code (zip)")]
+		public Link<_> SourceCodeZipLink { get; private set; }
+	}
+}
